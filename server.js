6acfe640c4add517c60effcf3b2bc207915bbc09
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-13 15:44:16 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req,res) {
+	res.sendFile(__dirname+'/index.html');
+});
+io.on('connection', function(socket) {
+	socket.on('message', function(data) {
+		if(data) {
+			io.emit('message','Message: '+data);
+		}
+	});
+});
+
+http.listen(3000);
