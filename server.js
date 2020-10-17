f2a80a7b83b8890173aa6aaa3735b02f5a690ee7
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-17 15:23:27 -0400

    Fix formatting

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', function(req,res) {
-	res.sendFile(__dirname+'/client.html');
+  res.sendFile(__dirname+'/client.html');
 });
 io.on('connection', function(socket) {
-	socket.on('message', function(data) {
-		if(data) {
-			io.emit('message','Message: '+data);
-		}
-	});
+  socket.on('message', function(data) {
+    if(data) {
+      io.emit('message','Message: '+data);
+    }
+   });
 });
 
 http.listen(3000);
